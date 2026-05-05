e40a99b7ee809722e1ecf9e30e46b75dedf7d94d
Tamas Gal <himself@tamasgal.com>
2026-05-05 15:26:44 +0200

    Add first prototype

--- src/Cutaway.jl
+++ src/Cutaway.jl
@@ -1,16 +1,45 @@
 module Cutaway
 
-export meaningoflife
+using StatsBase
 
-"""
-    meaningoflife()
+export CutHist, Cut
 
-Determines the meaning of life without floating point precision.
-"""
-function meaningoflife()
-    a = 21
-    b = 2
-    return a * b
+
+struct Cut
+    f::Function
+	label::AbstractString
+	description::AbstractString
+end
+
+
+struct CutHist{H<:StatsBase.AbstractHistogram} 
+    h::H
+	cuts::Vector{Cut}
+    hcuts::Vector{H}
+	function CutHist(h::H, cuts::Vector{Cut}) where H<:StatsBase.AbstractHistogram
+		n = length(cuts)
+		hcuts = Vector{H}()
+		for _ in 1:n
+			push!(hcuts, deepcopy(h))
+		end
+		new{H}(h, cuts, hcuts)
+	end
+end
+
+
+function Base.push!(chist::CutHist, x, context)
+    push!(chist.h, x)
+    for (cut, h) in zip(chist.cuts, chist.hcuts)
+		cut.f(context) || continue
+		push!(h, x)
+	end
+    chist
+end
+
+
+function Base.show(io::IO, c::CutHist)
+	n = length(c.cuts)
+	print(io, "CutHist($(n) cuts)")
 end
 
 end
